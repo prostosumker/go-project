8e1f02162c2a1dc585a0d2d1be583106dc0aa148
prostosumker <103947462+prostosumker@users.noreply.github.com>
2024-12-19 19:30:46 +0300

    Финальная отладка

--- Fundy/Backend/main.go
+++ Fundy/Backend/main.go
@@ -20,14 +20,49 @@ type Credentials struct {
 	Password string `json:"password"`
 }
 
+type outData struct {
+	ExpArray []int `json:"outExpArray"`
+	IncArray []int `json:"outIncArray"`
+}
+type inData struct {
+	ExpArray []int `json:"inpExpArray"`
+	IncArray []int `json:"inpIncArray"`
+}
+
 // непосредственная генерация jwt токена авторизации
 func generateToken(username string) (string, error) {
 	claims := &jwt.MapClaims{
 		"username": username,
-		"exp":      time.Now().Add(5 * time.Minute).Unix(),
+		"exp":      time.Now().Add(5 * time.Second).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtKey)
+}
+
+func extractUsernameFromToken(tokenString string) (string, error) {
+	// Распарсить токен
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Убедимся, что используется правильный метод подписи
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	// Проверить claims
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		username, ok := claims["username"].(string)
+		if !ok {
+			return "", fmt.Errorf("username not found in token")
+		}
+		return username, nil
+	}
+
+	return "", fmt.Errorf("invalid token")
 }
 
 // проверка введённых данных (при входе)
@@ -79,6 +114,78 @@ func checkLogPass(username string) bool {
 
 }
 
+// INSERT или UPDATE функция для записи значений
+func updInsFunc(userdatatable string, user_idi int, typo string, amount int) {
+	var existingAmount float64
+	connStr := "user=postgres password=1234 dbname=studydb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
+	}
+	defer db.Close()
+	switch userdatatable {
+	case "userdataexp":
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Сначала проверим, существует ли такой вид дохода для пользователя
+		err = tx.QueryRow("SELECT udi.amount FROM userdataexp AS udi WHERE udi.user_id = $1 AND type = $2", user_idi, typo).Scan(&existingAmount)
+		if err != nil && err != sql.ErrNoRows {
+			log.Fatal(err)
+		}
+
+		if err == sql.ErrNoRows {
+			// Если нет такого вида дохода, вставляем новый
+			_, err = tx.Exec("INSERT INTO userdataexp (user_id, type, amount) VALUES ($1, $2, $3)", user_idi, typo, amount)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			// Если такой доход уже есть, обновляем его
+			_, err = tx.Exec("UPDATE userdataexp SET amount = $1 WHERE user_id = $2 AND type = $3", amount, user_idi, typo)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		// Завершаем транзакцию
+		err = tx.Commit()
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "userdatainc":
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Сначала проверим, существует ли такой вид дохода для пользователя
+		err = tx.QueryRow("SELECT udi.amount FROM userdatainc AS udi WHERE udi.user_id = $1 AND type = $2", user_idi, typo).Scan(&existingAmount)
+		if err != nil && err != sql.ErrNoRows {
+			log.Fatal(err)
+		}
+
+		if err == sql.ErrNoRows {
+			// Если нет такого вида дохода, вставляем новый
+			_, err = tx.Exec("INSERT INTO userdatainc (user_id, type, amount) VALUES ($1, $2, $3)", user_idi, typo, amount)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			// Если такой доход уже есть, обновляем его
+			_, err = tx.Exec("UPDATE userdatainc SET amount = $1 WHERE user_id = $2 AND type = $3", amount, user_idi, typo)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		// Завершаем транзакцию
+		err = tx.Commit()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
 // функция генерации токена при входе в аккаунт
 func handleGenerateToken(w http.ResponseWriter, r *http.Request) {
 	fmt.Print("dsfgae")
@@ -111,7 +218,7 @@ func handleGenerateToken(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
 			Name:     "auth_token",
 			Value:    token,
-			Expires:  time.Now().Add(5 * time.Minute),
+			Expires:  time.Now().Add(15 * time.Second),
 			HttpOnly: true,
 			Secure:   false, // Используйте true, если работаете с HTTPS
 			Path:     "/",
@@ -143,7 +250,7 @@ func checkAuth(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     "auth_token",
 		Value:    tokenStr,
-		Expires:  time.Now().Add(10 * time.Second),
+		Expires:  time.Now().Add(15 * time.Second),
 		HttpOnly: true,
 		Secure:   false, // Используйте true, если работаете с HTTPS
 		Path:     "/",
@@ -155,6 +262,14 @@ func checkAuth(w http.ResponseWriter, r *http.Request) {
 
 // Функция загрузки данных в сайт
 func downloadingData(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+	var outputData = outData{
+		ExpArray: make([]int, 8),
+		IncArray: make([]int, 5),
+	}
 	cookie, err := r.Cookie("auth_token")
 	if err != nil {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
@@ -162,12 +277,205 @@ func downloadingData(w http.ResponseWriter, r *http.Request) {
 	}
 	tokenStr := cookie.Value
 	fmt.Println(tokenStr)
+	var username, errorr = extractUsernameFromToken(tokenStr)
+	if errorr != nil {
+		http.Error(w, "Ошибка обработки токена", http.StatusUnauthorized)
+		return
+	}
+	connStr := "user=postgres password=1234 dbname=studydb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
+	}
+	defer db.Close()
 
+	//Поиск данных затрат в базе данных
+	rows, err := db.Query("SELECT ude.type, ude.amount FROM public.userdataexp AS ude JOIN public.users AS u ON ude.user_id = u.user_id WHERE u.username = $1", username)
+	if err != nil {
+		log.Fatalf("Ошибка выполнения SELECT: %v", err)
+	}
+
+	for rows.Next() {
+		var typ string
+		var amount int
+		if err := rows.Scan(&typ, &amount); err != nil {
+			log.Fatalf("Ошибка чтения строки: %v", err)
+		}
+		fmt.Println(typ, amount)
+		switch typ {
+		case "food":
+			outputData.ExpArray[0] = amount
+			fmt.Println(outputData.ExpArray[0])
+		case "entertainment":
+			outputData.ExpArray[1] = amount
+		case "rent":
+			outputData.ExpArray[2] = amount
+		case "mortgage":
+			outputData.ExpArray[3] = amount
+		case "clothes":
+			outputData.ExpArray[4] = amount
+		case "beauty":
+			outputData.ExpArray[5] = amount
+		case "investment":
+			outputData.ExpArray[6] = amount
+		case "otherExp":
+			outputData.ExpArray[7] = amount
+		default:
+			fmt.Println("Не получилось обработать строку: ", typ)
+		}
+	}
+	rows.Close()
+
+	//Поиск данных доходов в базе данных
+	rowss, err := db.Query("SELECT udi.type, udi.amount FROM public.userDataInc AS udi JOIN public.users AS u ON udi.user_id = u.user_id WHERE u.username = $1", username)
+	if err != nil {
+		log.Fatalf("Ошибка выполнения SELECT: %v", err)
+	}
+
+	for rowss.Next() {
+		var typ string
+		var amount int
+		if err := rowss.Scan(&typ, &amount); err != nil {
+			log.Fatalf("Ошибка чтения строки: %v", err)
+		}
+		fmt.Println(typ, amount)
+		switch typ {
+		case "scholarship":
+			outputData.IncArray[0] = amount
+		case "paycheck":
+			outputData.IncArray[1] = amount
+		case "dividendts":
+			outputData.IncArray[2] = amount
+		case "deposit":
+			outputData.IncArray[3] = amount
+		case "otherInc":
+			outputData.IncArray[4] = amount
+		default:
+			fmt.Println("Не получилось обработать строку: ", typ)
+		}
+	}
+	rowss.Close()
+	http.SetCookie(w, &http.Cookie{
+		Name:     "auth_token",
+		Value:    tokenStr,
+		Expires:  time.Now().Add(15 * time.Second),
+		HttpOnly: true,
+		Secure:   false, // Используйте true, если работаете с HTTPS
+		Path:     "/",
+	})
+	fmt.Println(outputData)
+	jsonData, err := json.Marshal(outputData)
+	if err != nil {
+		log.Fatalf("Ошибка сериализации JSON: %v", err)
+	}
+	fmt.Println(string(jsonData)) // Выводим JSON перед отправкой
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 
 // Функция выгрузки данных из сайта
 func uploadingData(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+	var inputData = inData{
+		ExpArray: make([]int, 8),
+		IncArray: make([]int, 5),
+	}
+	var user_idi int
+	err := json.NewDecoder(r.Body).Decode(&inputData)
+	if err != nil {
+		http.Error(w, "Ошибка при разборе JSON", http.StatusBadRequest)
+		fmt.Println("Ошибка декодирования JSON:", err)
+		return
+	}
+	cookie, err := r.Cookie("auth_token")
+	if err != nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	tokenStr := cookie.Value
+	fmt.Println(tokenStr)
+	var username, errorr = extractUsernameFromToken(tokenStr)
+	if errorr != nil {
+		http.Error(w, "Ошибка обработки токена", http.StatusUnauthorized)
+		return
+	}
+	connStr := "user=postgres password=1234 dbname=studydb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
+	}
+	defer db.Close()
+	err = db.QueryRow("SELECT user_id FROM users WHERE username = $1", username).Scan(&user_idi)
+	if err != nil {
+		log.Fatalf("Ошибка прохождения селекта: %v", err)
+	}
 
+	// Вставляем или обновляем доходы
+	for i, amount := range inputData.IncArray {
+		var userdatatable = "userdatainc"
+		var typo string = ""
+		switch i {
+		case 0:
+			typo = "scholarship"
+			updInsFunc(userdatatable, user_idi, typo, amount)
+		case 1:
+			typo = "paycheck"
+			updInsFunc(userdatatable, user_idi, typo, amount)
+		case 2:
+			typo = "dividendts"
+			updInsFunc(userdatatable, user_idi, typo, amount)
+		case 3:
+			typo = "deposit"
+			updInsFunc(userdatatable, user_idi, typo, amount)
+		case 4:
+			typo = "otherInc"
+			updInsFunc(userdatatable, user_idi, typo, amount)
+		}
+		// Вставляем или обновляем расходы
+		for i, amount := range inputData.ExpArray {
+			var userdatatable = "userdataexp"
+
+			var typo string = ""
+			switch i {
+			case 0:
+				typo = "food"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			case 1:
+				typo = "entertainment"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			case 2:
+				typo = "rent"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			case 3:
+				typo = "mortgage"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			case 4:
+				typo = "clothes"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			case 5:
+				typo = "beauty"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			case 6:
+				typo = "investment"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			case 7:
+				typo = "otherExp"
+				updInsFunc(userdatatable, user_idi, typo, amount)
+			}
+		}
+
+	}
+	fmt.Println(inputData)
+	jsonData, err := json.Marshal(inputData)
+	if err != nil {
+		log.Fatalf("Ошибка сериализации JSON: %v", err)
+	}
+	fmt.Println(string(jsonData)) // Выводим JSON перед отправкой
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 
 // Функция работы с авторизованным пользователем (в разработке)
@@ -218,7 +526,7 @@ func signupFunc(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, &http.Cookie{
 			Name:     "auth_token",
 			Value:    token,
-			Expires:  time.Now().Add(5 * time.Minute),
+			Expires:  time.Now().Add(15 * time.Second),
 			HttpOnly: true,
 			Secure:   false, // Используйте true, если работаете с HTTPS
 			Path:     "/",
@@ -267,6 +575,8 @@ func main() {
 	r.HandleFunc("/welcome.html", authorizedFunc)
 	r.HandleFunc("/sign_up", signupFunc)
 	r.HandleFunc("/auth", checkAuth)
+	r.HandleFunc("/downloadingData", downloadingData)
+	r.HandleFunc("/uploadingData", uploadingData)
 	// Статическая раздача файлов
 	fs := http.FileServer(http.Dir("./../Frontend/"))
 	r.PathPrefix("/").Handler(http.StripPrefix("/", fs))
